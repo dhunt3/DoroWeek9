b75f44f6eaa2d22deab96d453fa7145c85f0d759
Noel <harrisbnoel@gmail.com>
2015-08-24 23:06:23 -0400

    Week 9 HW assignments - blog post, VS, Vocab terms

---
+++ IntString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWWeek9Doro
+{
+    class IntString
+    {
+        static void Main(string[] args)
+        {
+            int num = 23;
+            string result = IntStringConvert(num); // would not connect with the method under public string IntString
+                                            // would only work with static string IntString... maybe cuz it's in
+                                            // the same Program class.
+
+            Console.WriteLine("Your integer {0} is now converted to string {1}", num, result);
+            Console.ReadLine();
+        }
+
+        static string IntStringConvert(int num)
+        {
+            string numString = Convert.ToString(num);
+            return numString;
+        }
+
+    }
+}
---
+++ SquareRoot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRootDoro
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                SqrtFun();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + "**Error**");
+                Console.WriteLine("Method: " + e.TargetSite);
+                Console.WriteLine("Message: " + e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("\n" + "Goodbye");
+            }
+
+            Console.ReadLine();
+       }
+
+        public static void SqrtFun()
+        {
+            int num;
+            Console.WriteLine("Please enter a positive number: ");
+            bool result = int.TryParse(Console.ReadLine(), out num);
+
+            if (result == true && num >= 0)
+            {
+                double answer = Math.Sqrt(num);
+                Console.WriteLine("\n" + "The square root of {0} is {1}", num, answer);
+            }
+            
+            else
+                throw new Exception(string.Format("Your number is invalid!"));
+        }
+    }
+}
